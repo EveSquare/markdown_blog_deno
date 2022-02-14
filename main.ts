b0c56a1417be1144e92b37e8d248411fbf704838
evesquare <jonjonjonjon152@gmail.com>
2022-02-14 17:30:52 +0900

    check renderFileToString

--- main.ts
+++ main.ts
@@ -13,27 +13,53 @@ interface DirTree {
 }
 
 app.get("/", async (req) => {
-  return "hello debug"
+  // const startDir = `${cwd()}/pages`;
+  // const dirTree: DirTree = {};
+
+  // for await (const dirEntry of Deno.readDir(startDir)) {
+  //   if (dirEntry.isDirectory) {
+  //     let files = [];
+  //     for await (const file of Deno.readDir(path.join(startDir, dirEntry.name))) {
+  //       const fileInfo = {
+  //         name: file.name,
+  //         info: await Deno.stat(path.join(startDir, dirEntry.name, file.name))
+  //       }
+  //       files.push(fileInfo);
+  //     }
+  //     dirTree[dirEntry.name] = files;
+  //   }
+  // }
+
+  const output = await renderFileToString(`${cwd()}/views/debug.ejs`, {
+    title: "home",
+  });
+
+  return {
+    content: output,
+    headers: {
+      "Content-Type": "text/html"
+    }
+  };
 });
 
-// app.get("/:category/:page", async (req) => {
-//   const category = req.parameters.category;
-//   const page = req.parameters.page;
-//   const filePath = `${cwd()}/pages/${category}/${page}.md`;
+app.get("/:category/:page", async (req) => {
+  const category = req.parameters.category;
+  const page = req.parameters.page;
+  const filePath = `${cwd()}/pages/${category}/${page}.md`;
 
-//   const decoder = new TextDecoder("utf-8");
-//   const markdown = decoder.decode(await Deno.readFile(filePath));
-//   const markup = Marked.parse(markdown);
+  const decoder = new TextDecoder("utf-8");
+  const markdown = decoder.decode(await Deno.readFile(filePath));
+  const markup = Marked.parse(markdown);
 
-//   const output = await renderFileToString(`${cwd()}/views/pagesBase.ejs`, {
-//     title: page,
-//     body: markup.content
-//   });
+  const output = await renderFileToString(`${cwd()}/views/pagesBase.ejs`, {
+    title: page,
+    body: markup.content
+  });
   
-//   return {
-//     content: output,
-//     headers: {
-//       "Content-Type": "text/html"
-//     }
-//   };
-// });
+  return {
+    content: output,
+    headers: {
+      "Content-Type": "text/html"
+    }
+  };
+});
